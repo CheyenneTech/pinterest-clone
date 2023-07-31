583ca29dbb04b7086c30863ae6e25df0d28954ae
Cheyenne Edwards <112296933+CheyenneTech@users.noreply.github.com>
2023-07-31 19:02:06 -0400

    basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+
+
+// App.js
+import React, { useState } from 'react';
 import './App.css';
+import pinsData from './data'; // Assuming you have the 'data.js' file with the pins data.
+import Masonry from 'react-masonry-css';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 function App() {
+  const [pins, setPins] = useState(pinsData);
+
+  function handleLike(id) {
+    setPins((prevPins) =>
+      prevPins.map((pin) =>
+        pin.id === id ? { ...pin, isLiked: !pin.isLiked } : pin
+      )
+    );
+  }
+
+  function handleSave(id) {
+    setPins((prevPins) =>
+      prevPins.map((pin) =>
+        pin.id === id ? { ...pin, isSaved: !pin.isSaved } : pin
+      )
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Pinterest Lookalike</h1>
       </header>
+      <main>
+        <div className="grid">
+          {pins.map((pin) => (
+            <div key={pin.id} className="grid-item">
+              <img src={pin.image} alt={pin.title} />
+              <p>{pin.title}</p>
+              <div className="actions">
+                <button
+                  className={pin.isLiked ? 'liked' : ''}
+                  onClick={() => handleLike(pin.id)}
+                >
+                  {pin.isLiked ? 'Liked' : 'Like'}
+                </button>
+                <button
+                  className={pin.isSaved ? 'saved' : ''}
+                  onClick={() => handleSave(pin.id)}
+                >
+                  {pin.isSaved ? 'Saved' : 'Save'}
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+      <footer>
+        <p>Created by Cheyenne Edwards</p>
+        <p>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Terms of Service</a>
+          <a href="#">Contact Us</a>
+        </p>
+      </footer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,21 @@
+// data.js
+
+const pins = [
+  {
+    id: 1,
+    image: "plant.png",
+    title: "",
+    isLiked: false,
+    isSaved: false,
+  },
+  {
+    id: 2,
+    image: "flower.png",
+    title: "",
+    isLiked: false,
+    isSaved: false,
+  },
+  // Add more pins as needed
+];
+
+export default pins;
